71ff90d8002272ee1debf557370ea6f6e5138d6f
Oleksandr Tomesh <67783621+oleksandrtomesh@users.noreply.github.com>
2021-04-16 22:47:35 +0200

    added items to content

--- src/Components/Content.tsx
+++ src/Components/Content.tsx
@@ -1,19 +1,27 @@
 import { Grid } from "@material-ui/core";
 import React from "react";
+import { constants, ConstantsType } from "../constants";
 import { MyCard } from "./MyCard";
 
-export const Content: React.FC = (props) => {
+export const Content: React.FC<ConstantsType> = (props) => {
+  const cardItem = (data: ConstantsType) => {
+    const { title, price, description, avatarUrl, imageUrl } = data;
+    return (
+      <Grid item xs={12} sm={6}>
+        <MyCard
+          title={title}
+          price={price}
+          description={description}
+          avatarUrl={avatarUrl}
+          imageUrl={imageUrl}
+        />
+      </Grid>
+    );
+  };
+
   return (
     <Grid container direction="row" spacing={2}>
-      <Grid item xs={6} sm={4}>
-        <MyCard />
-      </Grid>
-      <Grid item xs={6} sm={4}>
-        <MyCard />
-      </Grid>
-      <Grid item xs={6} sm={4}>
-        <MyCard />
-      </Grid>
+      {constants.map((items) => cardItem(items))}
     </Grid>
   );
 };
--- src/Components/MyCard.tsx
+++ src/Components/MyCard.tsx
@@ -5,48 +5,32 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import { ConstantsType } from "../constants";
+import { Avatar, CardHeader, CardMedia, IconButton } from "@material-ui/core";
+import ShareIcon from "@material-ui/icons/Share";
 
-const useStyles = makeStyles({
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)"
-  },
-  title: {
-    fontSize: 14
-  },
-  pos: {
-    marginBottom: 12
-  }
-});
-export const MyCard: React.FC = (props) => {
-  const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
+export const MyCard: React.FC<ConstantsType> = (props) => {
+  const { title, price, description, avatarUrl, imageUrl } = props;
 
   return (
     <Card variant="outlined">
       <CardContent>
-        <Typography
-          className={classes.title}
-          color="textSecondary"
-          gutterBottom
-        >
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        <CardHeader
+          avatar={<Avatar src={avatarUrl} />}
+          action={
+            <IconButton aria-label="settings">
+              <ShareIcon />
+            </IconButton>
+          }
+          title={title}
+          subheader={price}
+        />
+        <CardMedia image={imageUrl} style={{ height: "300px" }} />
+        <Typography component="p">{description}</Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Learn More</Button>
+        <Button size="small">Buy Now</Button>
+        <Button size="small">Add to cart</Button>
       </CardActions>
     </Card>
   );
